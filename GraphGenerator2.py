276f8ba154d871f524e41bbbb33b3431785261ad
af9g19 <af9g19@soton.ac.uk>
2023-02-04 15:22:05 +0000

    GraphGen2 now works without weights

--- GraphGenerator2.py
+++ GraphGenerator2.py
@@ -9,21 +9,21 @@ import pygraphviz as pgv
 #Constraints ensure graph is Directed and acyclic
 
 class GraphGenerator:
-    def __init__(self,num_edges,num_paths,min_path_length,max_itr):
-        self.num_edges = num_edges
+    def __init__(self,num_nodes,num_paths,min_path_length,max_itr):
+        self.num_nodes = num_nodes
         self.num_paths = num_paths
         self.min_path_length = min_path_length
         self.max_itr = max_itr
 
     def init_graph(self,g):
         g.add_node(0,style='filled',fillcolor='red')
-        g.add_node(self.num_edges,style='filled',fillcolor='red')
-        for i in range(1,self.num_edges-1):
+        g.add_node(self.num_nodes,style='filled',fillcolor='red')
+        for i in range(1,self.num_nodes-1):
             g.add_node(i)
         return g
 
     def gen_graph(self):
-        print(self.num_edges)
+        print(self.num_nodes)
         g = nx.DiGraph()
         g = self.init_graph(g)
         i = 0
@@ -32,12 +32,17 @@ class GraphGenerator:
             i = i + 1
             fr_index = randrange(len(pointing))
             fr = pointing[fr_index]
-            to = random.randint(1,self.num_edges)
+            to = random.randint(1,self.num_nodes)
 
-            if(nx.is_path(g,[fr,to]) == True):
+            if(nx.is_path(g,[fr,to]) == True): #Ensure edge does not exist
                 continue
-            #if(nx.has_path(g,0,fr) == False):
-            #    continue
+            if(fr == 0 and to == self.num_nodes): #Ensures start node does not point to end
+                continue
+            if(fr == self.num_nodes): #Ensures final node is not pointing
+                continue
+            if((to == self.num_nodes) & (nx.shortest_path_length(g,0,fr) < self.min_path_length - 1)): #Ensure min path length holds
+                continue
+
 
             new_g = g.copy()
             new_g.add_edge(fr,to)
@@ -49,23 +54,33 @@ class GraphGenerator:
                 continue
             print(i)
         print('Complete')
+        path = list(nx.all_shortest_paths(g,0,self.num_nodes))
+        nodes_used = list(set().union(*path))
+        new_labels = range(0,len(nodes_used))
+        #MAKE DICT FOR RENAMING NODES TO CORRECT NAMES
+        mapping = {nodes_used[i]: new_labels[i] for i in range(len(nodes_used))}
+
+        for i in range(self.num_nodes):
+            if i not in nodes_used:
+                g.remove_node(i)
+        g = nx.relabel_nodes(g,mapping)
         A = nx.nx_agraph.to_agraph(g)
         A.layout(prog='dot')
         A.draw('test2.png')
         
     def total_paths(self,g):
         try:
-            path = list(nx.all_shortest_paths(g,0,self.num_edges))
+            path = list(nx.all_shortest_paths(g,0,self.num_nodes))
             filtered_paths = [x for x in path if len(x) >= self.min_path_length]
             print('PATH',path)
             print('FILTERED PATH', filtered_paths)
-            return len(path) 
+            return len(filtered_paths) 
         except:
             return 0     
 
     def current_min_path_length(self,g):
         try:
-            paths = list(nx.all_shortest_paths(g,0,self.num_edges))
+            paths = list(nx.all_shortest_paths(g,0,self.num_nodes))
             path_lengths = map(len(),paths)
             return min(path_lengths)
         except:
@@ -75,5 +90,5 @@ class GraphGenerator:
         return not nx.is_directed_acyclic_graph(g)
 
 if __name__ == "__main__":
-    f = GraphGenerator(20,1,5,300)
+    f = GraphGenerator(20,3,4,300)
     f.gen_graph()
